57b90757f14614113019c815404dfb9762073051
xeenlight <vovabvd@gmail.com>
2024-09-21 21:35:41 +0500

    2

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let themeToggleImg = document.getElementById('themeToggleImg');
 let isDarkTheme = true;
+let headerImg = document.querySelector('header img');
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (isDarkTheme) {
+        document.body.style.backgroundColor = '#101217';
+        document.body.style.color = '#ffffff';
+        document.querySelector('header').style.backgroundColor = '#1d1f25';
+        headerImg.classList.add('dark');
+    } else {
+        document.body.style.backgroundColor = '#fafbff';
+        document.body.style.color = '#1d1f25';
+        document.querySelector('header').style.backgroundColor = '#ebe9f4';
+        headerImg.classList.add('light');
+    }
+});
 
 // Функция для переключения тем
 function toggleTheme() {
@@ -11,18 +26,21 @@ function toggleTheme() {
         document.querySelector('header').style.backgroundColor = '#ebe9f4';
         document.querySelector('.gridBlocks').style.backgroundColor = '#ebe9f4';
         document.querySelector('.pointsBlocks').style.backgroundColor = '#ebe9f4';
-        
-        // Устанавливаем класс для .cell на светлой теме
+
         document.querySelectorAll('.cell').forEach(cell => {
             cell.classList.remove('dark');
             cell.classList.add('light');
         });
-        
+
         document.querySelectorAll('label').forEach(label => {
             label.classList.remove('dark-label');
             label.classList.add('light-label');
         });
-        
+
+        // Меняем класс для изображения
+        headerImg.classList.remove('dark');
+        headerImg.classList.add('light');
+
         isDarkTheme = false;
     } else {
         // Меняем цветовую тему на темную
@@ -32,18 +50,21 @@ function toggleTheme() {
         document.querySelector('header').style.backgroundColor = '#1d1f25';
         document.querySelector('.gridBlocks').style.backgroundColor = '#1d1f25';
         document.querySelector('.pointsBlocks').style.backgroundColor = '#1d1f25';
-        
-        // Устанавливаем класс для .cell на темной теме
+
         document.querySelectorAll('.cell').forEach(cell => {
             cell.classList.remove('light');
             cell.classList.add('dark');
         });
-        
+
         document.querySelectorAll('label').forEach(label => {
             label.classList.remove('light-label');
             label.classList.add('dark-label');
         });
-        
+
+        // Меняем класс для изображения
+        headerImg.classList.remove('light');
+        headerImg.classList.add('dark');
+
         isDarkTheme = true;
     }
 }
